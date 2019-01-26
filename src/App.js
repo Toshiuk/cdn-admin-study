f44f296e929b2e201e20be9ddf944c2ff05b4cd4
fjunior <fjunior@reweb.com.br>
2019-01-26 19:05:58 -0200

    Get list state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import './css/side-menu.css';
 import './css/pure-min.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      list : [{name:'Toshiuk',email:'flaviotoshiukhjr@outlook.com',password:'123456'}]
+    };
+  }
+
+
+
+
+
   render() {
     return (
       <div id="layout">
@@ -16,29 +28,29 @@ class App extends Component {
 
             <ul className="pure-menu-list">
               <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
+              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Author</a></li>
             </ul>
           </div>
         </div>
 
         <div id="main">
           <div className="header">
-            <h1>Cadastro de Autores</h1>
+            <h1>Author Register</h1>
           </div>
           <div className="content" id="content">
             <div className="pure-form pure-form-aligned">
               <form className="pure-form pure-form-aligned">
                 <div className="pure-control-group">
-                  <label htmlFor="nome">Nome</label>
-                  <input id="nome" type="text" name="nome" value="" />
+                  <label htmlFor="name">Name</label>
+                  <input id="name" type="text" name="name" value="" />
                 </div>
                 <div className="pure-control-group">
                   <label htmlFor="email">Email</label>
                   <input id="email" type="email" name="email" value="" />
                 </div>
                 <div className="pure-control-group">
-                  <label htmlFor="senha">Senha</label>
-                  <input id="senha" type="password" name="senha" />
+                  <label htmlFor="password">Password</label>
+                  <input id="password" type="password" name="password" />
                 </div>
                 <div className="pure-control-group">
                   <label></label>
@@ -56,10 +68,17 @@ class App extends Component {
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td>Toshiuk</td>
-                    <td>flaviotoshiukhjr@outloo.com</td>
-                  </tr>
+                  {
+                    this.state.list.map(function(author){
+                      return (
+                        <tr>
+                          <td>{author.name}</td>
+                          <td>{author.email}</td>
+                        </tr>
+                      );
+                    })
+                  }
+                 
                 </tbody>
               </table>
             </div>
